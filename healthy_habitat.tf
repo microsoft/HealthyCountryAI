710a05a3a5fc363949a196bfe14f0e00a2b00ae2
John Downs <john@johndowns.co.nz>
2019-09-26 16:42:00 +1000

    Temporarily add backend configuration for test purposes

--- healthy_habitat.tf
+++ healthy_habitat.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Deployment"
+    storage_account_name = "jodownscsirodeploy"
+    container_name       = "terraform"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
 # Configure the provider
 provider "azurerm" {
     version = "=1.27.0"
