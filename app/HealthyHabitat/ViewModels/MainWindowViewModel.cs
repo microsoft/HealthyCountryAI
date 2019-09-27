5bf7c5148fd51030eb3605d4f7f3d15e04b4d8d4
Cam Adams <caadam@microsoft.com>
2019-09-27 16:56:59 +1000

    Adding Victors intial az-copy code

--- app/HealthyHabitat/ViewModels/MainWindowViewModel.cs
+++ app/HealthyHabitat/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 {
     using GalaSoft.MvvmLight;
     using GalaSoft.MvvmLight.Command;
+    using Microsoft.Azure.Storage;
+    using Microsoft.Azure.Storage.Blob;
     using MvvmDialogs;
     using SixSeasons.Helpers;
     using SixSeasons.Models;
@@ -11,8 +13,10 @@
     using System.Collections.Specialized;
     using System.ComponentModel;
     using System.Configuration;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
+    using System.Text;
     using System.Windows;
     using System.Windows.Input;
 
@@ -152,6 +156,8 @@
                 ProgressVisible = true;
 
                 CopyFilesToLocalCache(this.Files.ToArray<string>(), this.SelectedLocation.Id, this.SelectedSeason);
+
+                InitiateAzCopy();
             });
 
             ExitCommand = new RelayCommand(() =>
@@ -174,7 +180,7 @@
 
         private async void CopyFilesToLocalCache(string[] files, string location, string season)
         {
-            string cacheLocation = System.Configuration.ConfigurationManager.AppSettings["localCache"].ToString() ?? AppDomain.CurrentDomain.BaseDirectory;
+            string cacheLocation = ConfigurationManager.AppSettings["localCache"].ToString() ?? AppDomain.CurrentDomain.BaseDirectory;
 
             Dictionary<string, string> fileCopyManifest = new Dictionary<string, string>();
 
@@ -200,6 +206,82 @@
             await AsyncFileCopy.CopyFiles(fileCopyManifest, prog => ProgressValue = prog);
 
             ProgressVisible = false;
+        }
+
+        private async void InitiateAzCopy()
+        {
+            StringBuilder command = new StringBuilder("azcopy cp \"[sourceDir]\" \"[sasUri]\" --recursive=true --put-md5");
+
+            string sourceDir = ConfigurationManager.AppSettings["localCache"] ?? AppDomain.CurrentDomain.BaseDirectory;
+            string account = ConfigurationManager.AppSettings["storageAccountName"];
+            string containerName = ConfigurationManager.AppSettings["containerName"];
+            string key = ConfigurationManager.AppSettings["key"];
+
+            CloudStorageAccount storageAccount = new CloudStorageAccount(
+                new Microsoft.Azure.Storage.Auth.StorageCredentials(account, key), true);
+
+            // Create a blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get a reference to a container named "mycontainer."
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            string sasUri = GetContainerSasUri(container);
+
+            command.Replace("[sourceDir]", sourceDir);
+            command.Replace("[sasUri]", sasUri);
+
+            string location = "cd " + AppDomain.CurrentDomain.BaseDirectory + "AzCopy";
+
+            using (Process cmd = new Process())
+            {
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+
+                cmd.Start();
+                cmd.StandardInput.WriteLine(location);
+                cmd.StandardInput.WriteLine(command);
+            }
+
+            // Todo
+            // Run Az-copy with one manifest per uplaod process. 
+            // Delete local cache
+        }
+
+        private static string GetContainerSasUri(CloudBlobContainer container, string storedPolicyName = null)
+        {
+            string sasContainerToken;
+
+            // If no stored policy is specified, create a new access policy and define its constraints.
+            if (storedPolicyName == null)
+            {
+                // Note that the SharedAccessBlobPolicy class is used both to define the parameters of an ad hoc SAS, and
+                // to construct a shared access policy that is saved to the container's shared access policies.
+                SharedAccessBlobPolicy adHocPolicy = new SharedAccessBlobPolicy()
+                {
+                    // When the start time for the SAS is omitted, the start time is assumed to be the time when the storage service receives the request.
+                    // Omitting the start time for a SAS that is effective immediately helps to avoid clock skew.
+                    SharedAccessStartTime = DateTime.UtcNow.AddHours(-24),
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                    Permissions = SharedAccessBlobPermissions.Write //| SharedAccessBlobPermissions.List
+                };
+
+                // Generate the shared access signature on the container, setting the constraints directly on the signature.
+                sasContainerToken = container.GetSharedAccessSignature(adHocPolicy, null);
+            }
+            else
+            {
+                // Generate the shared access signature on the container. In this case, all of the constraints for the
+                // shared access signature are specified on the stored access policy, which is provided by name.
+                // It is also possible to specify some constraints on an ad hoc SAS and others on the stored access policy.
+                sasContainerToken = container.GetSharedAccessSignature(null, storedPolicyName);
+            }
+
+            // Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
         }
     }
 }
