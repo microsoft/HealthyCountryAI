50682face96f355e51721c9ecb8f91c18ded746f
Steve Van Bodegraven <stvanbod@microsoft.com>
2020-04-24 19:14:06 +0930

    Fix Exception: ProgrammingError

--- functions_docker/score_regions/sql_database.py
+++ functions_docker/score_regions/sql_database.py
@@ -9,12 +9,12 @@ password = common.sql_database_password
 driver= '{ODBC Driver 17 for SQL Server}'
 
 def insert_animal_result(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box, logging):
-    statement = 'INSERT INTO dbo.Animals (DateOfFlight, LocationOfFlight, Season, RegionName, Label, Probability, URL, Latitude, Longitude, BoundingBox) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', {6}, \'{7}\', \'{8}\', \'{9}\')'.format(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box)
+    statement = 'INSERT INTO dbo.Animals (DateOfFlight, LocationOfFlight, Season, RegionName, Label, Probability, URL, Latitude, Longitude, BoundingBox) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\', \'{7}\', \'{8}\', \'{9}\')'.format(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box)
     logging.info(statement)
     execute(statement, logging)
 
 def insert_paragrass_result(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box, logging):
-    statement = 'INSERT INTO dbo.Habitat (DateOfFlight, LocationOfFlight, Season, RegionName, Label, Probability, URL, Latitude, Longitude, BoundingBox) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', {6}, \'{7}\', \'{8}\', \'{9}\')'.format(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box)
+    statement = 'INSERT INTO dbo.Habitat (DateOfFlight, LocationOfFlight, Season, RegionName, Label, Probability, URL, Latitude, Longitude, BoundingBox) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\', \'{6}\', \'{7}\', \'{8}\', \'{9}\')'.format(date_of_flight, location_of_flight, season, region_name, label, probability, url, latitude, longitude, bounding_box)
     logging.info(statement)
     execute(statement, logging)
     
