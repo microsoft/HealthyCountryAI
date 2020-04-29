4400d7516c4285e8bd8dee8f842b98cffb7a5113
Steve Van Bodegraven <stvanbod@microsoft.com>
2020-04-29 17:24:39 +0930

    (╯°□°）╯︵ ┻━┻

--- custom_function_app/score_regions/__init__.py
+++ custom_function_app/score_regions/__init__.py
@@ -51,9 +51,11 @@ def get_raster(data_path, container_name, date_of_flight, blob_name):
     return raster
 
 def get_latest_iteration(project_id):
+    logging.info('Project Id {0}'.format(project_id))
+
     iterations = custom_vision.get_iterations(project_id)
 
-    logging.info('Found Iteration {}'.format(iterations))
+    logging.info('Found Iteration {0}'.format(iterations))
 
     if len(iterations) > 0:
         return iterations.sort(reverse=True, key=lambda iteration: iteration.last_modified)[0]
